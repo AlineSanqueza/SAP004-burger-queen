fbd0009856551f237e5c87175423ab139e59628b
Aline <alinesanqueza@gmail.com>
2020-07-21 15:46:32 -0300

    reestruturação

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React from 'react';
-import Login from './pages/Login/login.js'
-//import Register from './pages/Register/register.js';
 
 //import Routes from './routes';
 
 const App = () => {
     return (
     <div>
-    <Login />
     {/*<Routes />*/}
     </div>
     )
