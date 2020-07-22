f031a25b9fd28d5f4f9aaa3f5a94b2af1011d1a8
marcellita <mteliceskidalpino@gmail.com>
2020-07-22 11:53:03 -0300

    tela login

--- src/Screen/Login.js
+++ src/Screen/Login.js
@@ -1,15 +1,20 @@
 import React from 'react';
+import logo from '../images/logo-burger-queen.png'
 import Input from '../components/Input';
 import Button from '../components/Button';
 import Paragraph from '../components/Phrase';
 import Footer  from '../components/Footer';
 
+import {StyleSheet,css} from 'aphrodite'
+
+
 const Login = () => {
   return (
     <div>
-      <Input title='E-mail'/>
+    < img   src = { logo } / >
+      <Input  style={css(styles.input)} title='E-mail'/>
       <Input title='Senha'/>
-      <Button children='Login'/>
+      <Button  style={css(styles.button)}children='Login'/>
       <Paragraph children='Não tem uma conta? Registre-se'/>
       <div>
         <Footer/>
@@ -17,5 +22,24 @@ const Login = () => {
     </div>
 );
 }
+
+const styles = StyleSheet.create({
+    input: {
+        background: 'rgba(154, 93, 38, 0.88)',
+        borderRadius: '5px'
+
+    },
+
+    button: {
+        background: 'rgba(217, 105, 7, 0.80)',
+        color:'rgba(242, 242, 242, 0.8)',
+        borderRadius: '40px',
+        padding: 10,
+        fontFamily: 'Spectral SC',
+        fontSize: '36px'
+
+    }
+
+})
 
 export default Login;
--- src/Screen/Register.js
+++ src/Screen/Register.js
@@ -1,25 +1,25 @@
-import React from 'react';
-import Input from '../components/Input';
-import Select from '../components/Select';
-import Button from '../components/Button';
-import Paragraph from '../components/Phrase';
-import Footer from '../components/Footer';
+//import React from 'react';
+//import Input from '../components/Input';
+//import Select from '../components/Select';
+//import Button from '../components/Button';
+//import Paragraph from '../components/Phrase';
+//import Footer from '../components/Footer';
 
-const Register = () => {
-  return (
-    <div>
-      <Paragraph children='Registro'/>
-      <Input title='Nome Completo'/>
-      <Input title='E-mail'/>
-      <Input title='Senha'/>
-      <Select/>
-      <Button children='Criar conta'/>
-      <Paragraph children='Já possui uma conta? Faça o login'/>
-      <div>
-        <Footer/>
-      </div>
-    </div>
-  );
-}
+//const Register = () => {
+//  return (
+//    <div>
+//      <Paragraph children='Registro'/>
+//      <Input title='Nome Completo'/>
+//      <Input title='E-mail'/>
+//      <Input title='Senha'/>
+//      <Select/>
+//      <Button children='Criar conta'/>
+//      <Paragraph children='Já possui uma conta? Faça o login'/>
+//      <div>
+//        <Footer/>
+//      </div>
+//    </div>
+//  );
+//}
 
-export default Register;
+//export default Register;
