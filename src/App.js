d35190fd08cd4cd240af2a26d92abaaf84c47921
marcellita <mteliceskidalpino@gmail.com>
2020-07-23 17:18:10 -0300

    CSS

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import './index.css';
-//import Login from './Screen/Login';
-import Register from './Screen/Register';
+import Login from './Screen/Login';
+//import Register from './Screen/Register';
 //import Routes from './routes';
 
 const App = () => {
   return (
     <>
-    {/*<Login />*/}
-    <Register />
+    <Login />
+    {/*<Register />*/}
     </>
   );
 }
