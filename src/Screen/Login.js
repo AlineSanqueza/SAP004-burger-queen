698f59dd321deaefc2ad7ebc7334c0e7e7f60570
marcellita <mteliceskidalpino@gmail.com>
2020-07-28 17:30:57 -0300

    Login funcionando

--- src/Screen/Login.js
+++ src/Screen/Login.js
@@ -1,19 +1,63 @@
-import React from 'react';
+import React,{ useState } from 'react';
 import Input from '../components/Input';
 import Logo from '../images/new-logo-burger.png';
 import Button from '../components/Button';
 import Footer from '../components/Footer';
 import { StyleSheet, css } from 'aphrodite';
-import { Link } from 'react-router-dom';
+import firebase from '../firebase';
+import { Link,useHistory } from 'react-router-dom';
+
 //import { queries } from '@testing-library/react';
 const Login = () => {
+    const history = useHistory();
+    const [email,setEmail] = useState('');
+    const [password,setPassword] = useState('');
+
+    const login = () => {
+        if (!email || !password){
+            alert('Verifique se todos os campos estão preenchidos');
+        } else{
+            //firebase.auth()
+            firebase
+            .auth()
+           .signInWithEmailAndPassword(email,password)
+           .then((user_id)=>{
+               firebase
+               .firestore()
+               .collection('users')
+               .doc(user_id.user.uid)
+               .get()
+               .then((doc) =>{
+                   if(doc.data().workplace === 'true'){
+                       history.push('/kitchen');
+                   }else {
+                        history.push('/saloon');
+                   }
+                   })
+                   .catch((error) => {
+                    const err = error.code
+                    if (err ==='auth/email-already-in-use') {
+                      alert('conta já existe');
+                    } else if (err === 'auth/invalid-email') {
+                      alert('email invalido');
+                    } else if (err === 'auth/weak-password') {
+                      alert('senha fraca');
+                    } else {
+                      alert('putz');
+                    }
+                  });
+               })
+            }
+        }
+
+
   return (
     <main className={css(styles.main)}>
       <form className={css(styles.form)}>
       <img className={css(styles.img)} src={Logo} alt='burger logo'/>
-        <Input style={css(styles.input)} type='e-mail' title='E-mail'/>
-        <Input style={css(styles.input)} type='password' title='Senha'/>
-        <Button style={css(styles.button)} children='Login'/>
+        <Input style={css(styles.input)} type='e-mail' title='E-mail' onChange={(e) => setEmail(e.target.value)} />
+        <Input style={css(styles.input)} type='password' title='Senha'  onChange={(e) => setPassword(e.target.value)}/>
+        <Button style={css(styles.button)} children='Login' onClick={(e) => {e.preventDefault(); login()}}/>
         <p className={css(styles.p)}> Não possui conta?
         <Link to='/register' className={css(styles.link)}> Registre-se</Link>
         </p>
@@ -22,12 +66,8 @@ const Login = () => {
     </main>
   );
 }
-/*const font = {
-  fontFamily: 'Spectral SC',
-  fontStyle: 'normal',
-  fontWeight: 'bold',
-  fontFamily: 'Arial'
-}*/
+    
+
 const styles = StyleSheet.create({
   main: {
     background: '#0D0D0D',
@@ -109,4 +149,5 @@ const styles = StyleSheet.create({
         color: '#F2F2F2'
     }
 });
+
 export default Login;
