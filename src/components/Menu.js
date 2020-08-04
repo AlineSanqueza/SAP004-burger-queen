eba329b480be6c988caa2c27402b2e118b4e986c
marcellita <mteliceskidalpino@gmail.com>
2020-08-04 20:07:33 -0300

    registro

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -5,8 +5,10 @@ function MenuButton(props) {
     return(
         // passar a função onclick como props. 
         <button onClick={props.onClink}>
+                        <>
                         <p>{item.name}</p>
                         <p>R${item.price},00</p>
+                        </>
         </button>
     )
 }
